900fbab2d4c0d5770b8863af2e2c3cecbb842101
Meet <149648810+Meet-official@users.noreply.github.com>
2024-12-07 02:42:05 +0530

    Update profile.php

--- Profile/profile.php
+++ Profile/profile.php
@@ -1,9 +1,9 @@
 <?php
 // Database credentials
-$host = "sql201.infinityfree.com";
-$dbname = "if0_37800644_profile";
-$user = "if0_37800644";
-$pass = "h1xAuBfpYD";
+$host = "sql306.infinityfree.com";
+$dbname = "if0_37839418_yogasan";
+$user = "if0_37839418";
+$pass = "YOGJGUNI";
 $port = 3306;
 
 // Start the session
